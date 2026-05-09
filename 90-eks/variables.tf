1631577b1043aa3d45053888c9d4563d9a1608f4
heman chandra <hemanchandra701@gmail.com>
2026-05-09 23:07:45 +0530

    k8s

--- 90-eks/variables.tf
+++ 90-eks/variables.tf
@@ -7,17 +7,21 @@ variable "environment" {
 }
 
 variable "eks_version" {
+    default = 1.33
 }
 
 variable "enable_blue" {
     type    = bool
+    default = "true"
 }
 
 variable "eks_nodegroup_blue_version" {
+    default = 1.33
 }
 
 variable "enable_green" {
     type    = bool
+    default = "false"
 }
 
 variable "eks_nodegroup_green_version" {
