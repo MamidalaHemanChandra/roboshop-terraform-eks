24cbdd40f7639b5bd070ea1a0fe780a3d3beca6a
heman chandra <hemanchandra701@gmail.com>
2026-05-31 10:08:36 +0530

    jenkins

--- 90-eks/variables.tf
+++ 90-eks/variables.tf
@@ -6,23 +6,23 @@ variable "environment" {
     default = "dev"
 }
 
-variable "eks_version" {
-    default = 1.33
-}
+# variable "eks_version" {
+#     default = 1.33
+# }
 
-variable "enable_blue" {
-    type    = bool
-    default = "true"
-}
+# variable "enable_blue" {
+#     type    = bool
+#     default = "true"
+# }
 
-variable "eks_nodegroup_blue_version" {
-    default = 1.33
-}
+# variable "eks_nodegroup_blue_version" {
+#     default = 1.33
+# }
 
-variable "enable_green" {
-    type    = bool
-    default = "false"
-}
+# variable "enable_green" {
+#     type    = bool
+#     default = "false"
+# }
 
-variable "eks_nodegroup_green_version" {
-}
+# variable "eks_nodegroup_green_version" {
+# }
