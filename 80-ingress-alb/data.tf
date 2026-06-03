bcf4ff331f15215b32e7e8b7507765c33e92d547
heman chandra <hemanchandra701@gmail.com>
2026-06-03 18:46:37 +0530

    jenkins

--- 80-ingress-alb/data.tf
+++ 80-ingress-alb/data.tf
@@ -9,3 +9,7 @@ data "aws_ssm_parameter" "public_subnet_ids" {
 data "aws_ssm_parameter" "ingress_certificate_arn" {
   name = "/${var.project}/${var.environment}/ingress_certificate_arn"
 }
+
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project}/${var.environment}/vpc_id"
+}
